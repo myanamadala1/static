281262761ca829cf18734a5bd24a6278a10cb328
Abhilash <vabhila@americas.corpdir.net>
2019-01-07 11:06:11 -0500

    first

--- test/integration_test_helper.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'test_helper'
-require 'rack/test'
-require 'capybara/rails'
-
-class ActionDispatch::IntegrationTest < ActiveSupport::TestCase
-  include Rack::Test::Methods
-  include Capybara::DSL
-end
